f568266a7d9f3a0060452575165a0650ba35b158
meakitfed <nathan.bonnard@hotmail.fr>
2018-12-10 22:44:54 +0100

    Bootstrap importé et marche

--- IntranetPOPS1819/Global.asax.cs
+++ IntranetPOPS1819/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace IntranetPOPS1819
@@ -12,7 +13,8 @@ namespace IntranetPOPS1819
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
     }
 }
