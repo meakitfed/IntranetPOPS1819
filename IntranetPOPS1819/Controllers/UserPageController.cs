df87a41d2ae65f1329ebff32fed9b1b931d4b2fc
userBrian <brian.martin@u-psud.fr>
2018-12-13 13:21:54 +0100

    hope

---
+++ IntranetPOPS1819/Controllers/UserPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntranetPOPS1819.Controllers
+{
+    public class UserPageController : Controller
+    {
+        // GET: UserPage
+        public ActionResult Profil()
+        {
+            return View();
+        }
+    }
+}
