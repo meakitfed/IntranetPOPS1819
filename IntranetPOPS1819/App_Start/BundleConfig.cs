b6a963841cada203e9cbac9f3dca91eb0b85df1c
meakitfed <nathan.bonnard@hotmail.fr>
2019-02-16 10:24:48 +0100

    Telerik ajouté, aller dans Grid/Index pour tester une grid

--- IntranetPOPS1819/App_Start/BundleConfig.cs
+++ IntranetPOPS1819/App_Start/BundleConfig.cs
@@ -8,7 +8,9 @@ namespace IntranetPOPS1819
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/Scripts/jquery-{version}.js",
+						"~/Scripts/jquery.validate.min.js",
+						"~/Scripts/jquery.validate.unobtrusive.min.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
 						"~/Scripts/jquery.validate*"));
---
+++ IntranetPOPS1819/Controllers/GridController.cs
@@ -0,0 +1,133 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using IntranetPOPS1819.Models;
+
+namespace IntranetPOPS1819.Controllers
+{
+    public class GridController : Controller
+    {
+        private BddContext db = new BddContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Collaborateurs_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<Collaborateur> collaborateurs = db.Collaborateurs;
+            DataSourceResult result = collaborateurs.ToDataSourceResult(request, collaborateur => new {
+                Id = collaborateur.Id,
+                Mail = collaborateur.Mail,
+                MotDePasse = collaborateur.MotDePasse,
+                Nom = collaborateur.Nom,
+                Prenom = collaborateur.Prenom,
+                CongésRestants = collaborateur.CongésRestants,
+                Admin = collaborateur.Admin,
+                Chef = collaborateur.Chef,
+                Telephone = collaborateur.Telephone,
+                LastUpdate = collaborateur.LastUpdate,
+                LastUpdateNoteDeFrais = collaborateur.LastUpdateNoteDeFrais
+            });
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Collaborateurs_Create([DataSourceRequest]DataSourceRequest request, Collaborateur collaborateur)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Collaborateur
+                {
+                    Mail = collaborateur.Mail,
+                    MotDePasse = collaborateur.MotDePasse,
+                    Nom = collaborateur.Nom,
+                    Prenom = collaborateur.Prenom,
+                    CongésRestants = collaborateur.CongésRestants,
+                    Admin = collaborateur.Admin,
+                    Chef = collaborateur.Chef,
+                    Telephone = collaborateur.Telephone,
+                    LastUpdate = collaborateur.LastUpdate,
+                    LastUpdateNoteDeFrais = collaborateur.LastUpdateNoteDeFrais
+                };
+
+                db.Collaborateurs.Add(entity);
+                db.SaveChanges();
+                collaborateur.Id = entity.Id;
+            }
+
+            return Json(new[] { collaborateur }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Collaborateurs_Update([DataSourceRequest]DataSourceRequest request, Collaborateur collaborateur)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Collaborateur
+                {
+                    Id = collaborateur.Id,
+                    Mail = collaborateur.Mail,
+                    MotDePasse = collaborateur.MotDePasse,
+                    Nom = collaborateur.Nom,
+                    Prenom = collaborateur.Prenom,
+                    CongésRestants = collaborateur.CongésRestants,
+                    Admin = collaborateur.Admin,
+                    Chef = collaborateur.Chef,
+                    Telephone = collaborateur.Telephone,
+                    LastUpdate = collaborateur.LastUpdate,
+                    LastUpdateNoteDeFrais = collaborateur.LastUpdateNoteDeFrais
+                };
+
+                db.Collaborateurs.Attach(entity);
+                db.Entry(entity).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Json(new[] { collaborateur }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Collaborateurs_Destroy([DataSourceRequest]DataSourceRequest request, Collaborateur collaborateur)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Collaborateur
+                {
+                    Id = collaborateur.Id,
+                    Mail = collaborateur.Mail,
+                    MotDePasse = collaborateur.MotDePasse,
+                    Nom = collaborateur.Nom,
+                    Prenom = collaborateur.Prenom,
+                    CongésRestants = collaborateur.CongésRestants,
+                    Admin = collaborateur.Admin,
+                    Chef = collaborateur.Chef,
+                    Telephone = collaborateur.Telephone,
+                    LastUpdate = collaborateur.LastUpdate,
+                    LastUpdateNoteDeFrais = collaborateur.LastUpdateNoteDeFrais
+                };
+
+                db.Collaborateurs.Attach(entity);
+                db.Collaborateurs.Remove(entity);
+                db.SaveChanges();
+            }
+
+            return Json(new[] { collaborateur }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
