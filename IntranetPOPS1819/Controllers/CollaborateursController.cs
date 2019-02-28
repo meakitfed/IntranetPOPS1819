7dee06bc5062c65e781c345a7124ba154917a2bc
Chaferfu <mathba@gmx.com>
2019-02-28 18:42:55 +0100

    Cree la vue des collabo

---
+++ IntranetPOPS1819/Controllers/CollaborateursController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntranetPOPS1819.Controllers
+{
+    public class CollaborateursController : Controller
+    {
+        // GET: Collaborateurs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
