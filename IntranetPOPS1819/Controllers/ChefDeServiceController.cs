c6fb4d4af0fe630598dc2f33b9d174fe4072efb2
Brian <brian78120@gmail.com>
2019-03-05 17:31:15 +0100

    division onglet mission

--- IntranetPOPS1819/Controllers/ChefDeServiceController.cs
+++ IntranetPOPS1819/Controllers/ChefDeServiceController.cs
@@ -139,19 +139,7 @@ namespace IntranetPOPS1819.Controllers
 					vm.nbrEnAttente.Add(b);
 				}
 				
-                vm.Missions = new List<MissionsViewModel>();
-
-                IEnumerable<SelectListItem> collabosList =
-                                from category in dal.ObtenirTousLesCollaborateurs()
-                                select new SelectListItem
-                                {
-                                    Text = category.Nom,
-                                    Value = category.Id.ToString()
-                                };
-                ViewData["collabos"] = collabosList;
-
-                foreach (Mission m in vm._Collaborateur.Service.Missions)
-                    vm.Missions.Add(new MissionsViewModel { Id = m.Id, Nom = m.Nom});
+                
 				return View(vm);
 			}
 			return View();
@@ -275,86 +263,6 @@ namespace IntranetPOPS1819.Controllers
             }
             
             return View();
-        }
-
-        public ActionResult MissionEditing_Read([DataSourceRequest]DataSourceRequest request)
-        {
-            Debug.WriteLine("Passage dans MissionEditing_Read");
-
-            IDal dal = new Dal();
-
-            List<MissionsViewModel> missions = new List<MissionsViewModel>();
-            foreach (Mission m in dal.ObtenirCollaborateur(HttpContext.User.Identity.Name).Service.Missions)
-            {
-                List<IdentiteViewModel> collabos = new List<IdentiteViewModel>();
-                foreach (Collaborateur c in m.Collaborateurs)
-                    collabos.Add(new IdentiteViewModel { Nom = c.Prenom + " " + c.Nom, Id = c.Id });
-
-                missions.Add(new MissionsViewModel { Id = m.Id, Nom = m.Nom, Collaborateurs = collabos });
-            }
-            if(missions.Count() > 0) { 
-                Debug.WriteLine(missions[0].Collaborateurs.Count() + " collabos associés à cette mission");
-
-            missions[0].Collabs.Add("Bob");
-            missions[0].Collabs.Add("Hnery");
-            }
-            IQueryable<MissionsViewModel> liste = missions.AsQueryable();
-
-            DataSourceResult result = liste.ToDataSourceResult(request, data => new
-            {
-                data.Id,
-                data.Nom,
-                data.Statut,
-                data.Collaborateurs,
-                data.Collabs
-            });
-
-            return Json(result);
-        }
-        
-        public ActionResult MissionsEditingCreate(MissionsViewModel m)
-        {
-
-
-            return View();
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult MissionEditing_Update([DataSourceRequest] DataSourceRequest request, MissionsViewModel m)
-        {
-            Debug.WriteLine("Passage update");
-            Debug.WriteLine(m.Temp.Count());
-            foreach(SelectListItem c in m.Temp)
-            {
-                if (c == null) Debug.WriteLine("suicide");
-                else
-                    Debug.WriteLine(c.Text + c.Value);
-            }
-
-            IDal dal = new Dal();
-
-            dal.UpdateMission(m);
-
-            return View();
-        }
-
-        public JsonResult GetCollaborateurs(string text)
-        {
-            IDal dal = new Dal();
-            Debug.WriteLine("Passage dans GetCollaborateurs");
-            if (HttpContext.User.Identity.IsAuthenticated)
-            {
-                var collabos = dal.ObtenirCollaborateursService(dal.ObtenirCollaborateur(HttpContext.User.Identity.Name).Service.Id).Select(c => new Collaborateur
-                {
-                    Id = c.Id,
-                    Nom = c.Nom
-                });
-
-                Debug.WriteLine(collabos.Count());
-
-                return Json(collabos, JsonRequestBehavior.AllowGet);
-            }
-            return null;
         }
 
         public int InfosAbsents(DateTime date)
